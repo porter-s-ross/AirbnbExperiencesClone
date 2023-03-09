946001ea81629649ce18d01356c15e75fb085f93
rss <opqlns@pm.me>
2023-03-09 16:55:19 -0600

    init commit

---
+++ components/Card.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+export default function Card(props) {
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
+    return (
+        <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.item.coverImg}`} className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
+            </div>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
+        </div>
+    )
+}
+
+
+ // key={item.id}
+    //             img={item.coverImg}
+    //             rating={item.stats.rating}
+    //             reviewCount={item.stats.reviewCount}
+    //             location={item.location}
+    //             title={item.title}
+    //             price={item.price}
+    //             openSpots={item.openSpots}
